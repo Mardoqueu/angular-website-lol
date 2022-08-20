f09be7eb01021100232f7e79ee58b8991156cfe5
mardoqueu <mdq767@gmail.com>
2022-08-20 19:18:58 -0300

    Fixing the memory leak

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -21,6 +21,12 @@ export class ModalService {
     })
   }
 
+  unregister(id: string) {
+    this.modals = this.modals.filter(
+      element => element.id !== id
+    )
+  }
+
   isModalOpen(id: string): boolean{
     return !!this.modals.find(element => element.id == id)?.visible
   }
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,13 +6,16 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './auth-modal.component.html',
   styleUrls: ['./auth-modal.component.css']
 })
-export class AuthModalComponent implements OnInit {
+export class AuthModalComponent implements OnInit, OnDestroy {
 
   constructor(public modal: ModalService) { }
 
   ngOnInit(): void {
     this.modal.register('auth')
+  }
 
+  ngOnDestroy(){
+    this.modal.unregister('auth')
   }
 
 }
